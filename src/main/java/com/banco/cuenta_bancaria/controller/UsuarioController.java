54188cc1ce86dc36c53b1eceff5a093eec1acbb9
Charlinson <charlinson2013@gmail.com>
2025-01-08 17:23:01 -0500

    feat: crea controller para registrar usuarios

---
+++ src/main/java/com/banco/cuenta_bancaria/controller/UsuarioController.java
@@ -0,0 +1,40 @@
+package com.banco.cuenta_bancaria.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.banco.cuenta_bancaria.dto.request.ActualizarUsuarioRequestDTO;
+import com.banco.cuenta_bancaria.dto.request.RegistrarUsuarioRequestDTO;
+import com.banco.cuenta_bancaria.dto.response.ResponseDTO;
+import com.banco.cuenta_bancaria.service.IUsuarioService;
+import com.banco.cuenta_bancaria.util.Result;
+
+import jakarta.validation.Valid;
+
+
+@RestController
+@RequestMapping("/api/v1/usuario")
+@CrossOrigin(origins = "http://localhost:4200")
+public class UsuarioController {
+
+    private final IUsuarioService usuarioService;
+
+    public UsuarioController(IUsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<ResponseDTO<String>> registrarUsuario(@Valid @RequestBody RegistrarUsuarioRequestDTO request) {
+        Result<String, String> result = usuarioService.registrarUsuario(request);
+
+        return result.isSuccess()
+					? ResponseEntity.status(HttpStatus.OK).body(ResponseDTO.<String>builder()
+							.message(result.getValue())
+							.code(HttpStatus.OK.value()).response(null).build())
+					: ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(ResponseDTO.<String>builder()
+							.message(String.join("\n", result.getErrors()))
+							.code(result.getStatusCode().value()).response(null).build());
+    }
+}
