fdbe56b96018e0c991db772f0fdb0182cca8ed44
Charlinson <charlinson2013@gmail.com>
2025-01-08 17:29:22 -0500

    feat: crea repository para usuario

---
+++ src/main/java/com/banco/cuenta_bancaria/repository/IUsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.banco.cuenta_bancaria.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.banco.cuenta_bancaria.entity.Usuario;
+
+@Repository
+public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
+}
