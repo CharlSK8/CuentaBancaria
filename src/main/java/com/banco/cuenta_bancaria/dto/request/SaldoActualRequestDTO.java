3030cd93301111de9079c03d1b431b3b669ae6aa
Charlinson <charlinson2013@gmail.com>
2025-01-08 20:12:32 -0500

    feat: crea dto para saldo actual en cuenta bancaria

---
+++ src/main/java/com/banco/cuenta_bancaria/dto/request/SaldoActualRequestDTO.java
@@ -0,0 +1,16 @@
+package com.banco.cuenta_bancaria.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaldoActualRequestDTO {
+
+    private String numeroCuenta;
+
+}
