8a26faeb349b94b2b68f4643f2507c8cd1eb37f8
Charlinson <charlinson2013@gmail.com>
2025-01-09 17:50:18 -0500

    feat: crea ITokenRepository

---
+++ src/main/java/com/banco/cuenta_bancaria/repository/ITokenRepository.java
@@ -0,0 +1,13 @@
+package com.banco.cuenta_bancaria.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.banco.cuenta_bancaria.entity.Token;
+
+@Repository
+public interface ITokenRepository extends JpaRepository<Token, Long> {
+}
