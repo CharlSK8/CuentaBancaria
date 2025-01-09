f523711b969c35087e6553f525e92934f9dc0192
Charlinson <charlinson2013@gmail.com>
2025-01-08 20:01:53 -0500

    feat: crea repository para cuenta bancaria

---
+++ src/main/java/com/banco/cuenta_bancaria/repository/ICuentaBancariaRepository.java
@@ -0,0 +1,12 @@
+package com.banco.cuenta_bancaria.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.banco.cuenta_bancaria.entity.CuentaBancaria;
+
+@Repository
+public interface ICuentaBancariaRepository extends JpaRepository<CuentaBancaria, Long>{
+}
