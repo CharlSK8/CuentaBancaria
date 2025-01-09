eb08d03cbdf35048f68b5451af48abceb1f8c9f2
Charlinson <charlinson2013@gmail.com>
2025-01-08 20:27:38 -0500

    feat: se crea entidad CuentaBancaria

---
+++ src/main/java/com/banco/cuenta_bancaria/entity/CuentaBancaria.java
@@ -0,0 +1,44 @@
+package com.banco.cuenta_bancaria.entity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cuenta_bancaria")
+public class CuentaBancaria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int numeroCuenta;
+    private BigDecimal saldo;
+    private String tipoCuenta;
+    private boolean activa;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "cuentaBancaria", cascade = CascadeType.ALL)
+    private List<Movimiento> movimientos;
+
+}
