eb1e6d622ae1f3c38dd92a142e8439cf81c8ac0f
Charlinson <charlinson2013@gmail.com>
2025-01-08 20:08:49 -0500

    feat: crea dto para crear cuenta bancaria

---
+++ src/main/java/com/banco/cuenta_bancaria/dto/request/CrearCuentaRequestDTO.java
@@ -0,0 +1,30 @@
+package com.banco.cuenta_bancaria.dto.request;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class CrearCuentaRequestDTO {
+
+    @NotNull(message = "El campo 'numeroCuenta' es obligatorio")
+    private int numeroCuenta;
+    @NotBlank(message = "El campo 'saldo' es obligatorio")
+    private BigDecimal saldo;
+    @NotBlank(message = "El campo 'tipoCuenta' es obligatorio")
+    @Pattern(regexp = "^(CORRIENTE|AHORROS)$", message = "El tipo de cuenta debe ser 'CORRIENTE' o 'AHORROS'")
+    private String tipoCuenta;
+    @NotBlank(message = "El campo 'numeroIdetificacion' es obligatorio")
+    private String numeroIdetificacion;
+    
+}
+
