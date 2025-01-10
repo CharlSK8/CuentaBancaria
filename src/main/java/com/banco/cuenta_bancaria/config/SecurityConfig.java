e03d665a78b1aa21f9d2122c5c57ffec6b016e42
Charlinson <charlinson2013@gmail.com>
2025-01-10 13:48:24 -0500

    feat: crea SecurityFilterChain, define la cadena de filtros de seguridad para peticiones HTTP entrantes

---
+++ src/main/java/com/banco/cuenta_bancaria/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.banco.cuenta_bancaria.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.banco.cuenta_bancaria.component.JwtAuthFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                            "api/v1/auth/**",
+                            "/docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html", 
+                            "/docs/swagger-ui/**", 
+                            "/v3/api-docs/**",
+                            "/h2-console/**"  
+                        ).permitAll()
+                        .requestMatchers("/api/v1/movimiento").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .headers(headers -> headers.frameOptions().disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
