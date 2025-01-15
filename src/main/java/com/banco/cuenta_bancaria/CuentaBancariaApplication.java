f8c667193bb7863562b3422949dc577fc29bab6b
Charlinson <charlinson2013@gmail.com>
2025-01-14 20:02:28 -0500

    feat: habilita JPA Auditing

--- src/main/java/com/banco/cuenta_bancaria/CuentaBancariaApplication.java
+++ src/main/java/com/banco/cuenta_bancaria/CuentaBancariaApplication.java
@@ -2,7 +2,9 @@ package com.banco.cuenta_bancaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CuentaBancariaApplication {
 
