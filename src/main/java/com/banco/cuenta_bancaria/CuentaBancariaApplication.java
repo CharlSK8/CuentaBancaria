0ec30c13a9b636962bf3133cc7aecaff3153ea70
Charlinson <charlinson2013@gmail.com>
2025-01-27 17:28:15 -0500

    feat: habilita swagger desde la aplicación inicial

--- src/main/java/com/banco/cuenta_bancaria/CuentaBancariaApplication.java
+++ src/main/java/com/banco/cuenta_bancaria/CuentaBancariaApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Operaciones Bancarias API", version = "1.0.0", description = "API para operaciones bancarias"))
 public class CuentaBancariaApplication {
 
 	public static void main(String[] args) {
