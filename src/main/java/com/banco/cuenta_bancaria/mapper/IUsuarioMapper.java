dfbb84409cdc9a6d9e202e4bb2764705c3d04bf2
Charlinson <charlinson2013@gmail.com>
2025-01-29 14:52:01 -0500

    feat: crea mapper para convertir de Usuario a UsuarioResponseDTO

--- src/main/java/com/banco/cuenta_bancaria/mapper/IUsuarioMapper.java
+++ src/main/java/com/banco/cuenta_bancaria/mapper/IUsuarioMapper.java
@@ -1,14 +1,30 @@
 package com.banco.cuenta_bancaria.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import com.banco.cuenta_bancaria.dto.request.RegistrarUsuarioRequestDTO;
+import com.banco.cuenta_bancaria.dto.response.CuentaBancariaResponseDTO;
+import com.banco.cuenta_bancaria.dto.response.UsuarioResponseDTO;
+import com.banco.cuenta_bancaria.entity.CuentaBancaria;
 import com.banco.cuenta_bancaria.entity.Usuario;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface IUsuarioMapper {
 
     Usuario toUsuario(RegistrarUsuarioRequestDTO registrarUsuarioRequestDTO);
+    
+    @Mapping(source = "numeroCuenta", target = "numeroCuenta")
+    @Mapping(source = "saldo", target = "saldo")
+    @Mapping(source = "tipoCuenta", target = "tipoCuenta")
+    @Mapping(source = "activa", target = "activa")
+    CuentaBancariaResponseDTO toCuentaBancariaResponseDTO(CuentaBancaria cuentaBancaria);
+
+    List<CuentaBancariaResponseDTO> toCuentaBancariaResponseDTOs(List<CuentaBancaria> cuentasBancarias);
+
+    UsuarioResponseDTO toResponseDTO(Usuario usuario);
 
 }
