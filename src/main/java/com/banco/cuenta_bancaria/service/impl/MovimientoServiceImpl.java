61d252e362b27b24bcfc3172f70f26cb65cb816b
Charlinson <charlinson2013@gmail.com>
2025-01-29 18:24:06 -0500

    style: elimina importaciones innecesarias

--- src/main/java/com/banco/cuenta_bancaria/service/impl/MovimientoServiceImpl.java
+++ src/main/java/com/banco/cuenta_bancaria/service/impl/MovimientoServiceImpl.java
@@ -3,7 +3,6 @@ package com.banco.cuenta_bancaria.service.impl;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
