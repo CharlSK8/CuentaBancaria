659e987af54703a5c76fe1280ebaa3bc99cd01d4
Charlinson <charlinson2013@gmail.com>
2025-01-29 10:30:08 -0500

    refactor: elimina validaciones y creación de instancias en el servicio

--- src/main/java/com/banco/cuenta_bancaria/service/impl/CuentaBancariaServiceImpl.java
+++ src/main/java/com/banco/cuenta_bancaria/service/impl/CuentaBancariaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.banco.cuenta_bancaria.service.impl;
 
 import com.banco.cuenta_bancaria.service.ICuentaBancariaService;
+import com.banco.cuenta_bancaria.util.CuentaBancariaUtil;
 import com.banco.cuenta_bancaria.util.Result;
 
 import lombok.RequiredArgsConstructor;
@@ -10,18 +11,19 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.banco.cuenta_bancaria.dto.request.CrearCuentaRequestDTO;
 import com.banco.cuenta_bancaria.dto.request.DepositoCuentaRequestDTO;
 import com.banco.cuenta_bancaria.dto.request.RetiroCuentaRequestDTO;
+import com.banco.cuenta_bancaria.dto.response.CuentaBancariaCreadaResponse;
 import com.banco.cuenta_bancaria.dto.response.SaldoActualResponseDTO;
 import com.banco.cuenta_bancaria.entity.CuentaBancaria;
 import com.banco.cuenta_bancaria.entity.Movimiento;
 import com.banco.cuenta_bancaria.entity.Usuario;
 import com.banco.cuenta_bancaria.enums.TipoMovimiento;
+import com.banco.cuenta_bancaria.mapper.ICuentaBancariaMapper;
 import com.banco.cuenta_bancaria.repository.ICuentaBancariaRepository;
 import com.banco.cuenta_bancaria.repository.IMovimientoRepository;
 import com.banco.cuenta_bancaria.repository.IUsuarioRepository;
@@ -33,29 +35,20 @@ public class CuentaBancariaServiceImpl implements ICuentaBancariaService {
     private final ICuentaBancariaRepository cuentaBancariaRepository;
     private final IUsuarioRepository usuarioRepository;
     private final IMovimientoRepository movimientoRepository;
+    private final ICuentaBancariaMapper cuentaBancariaMapper;
+    private final CuentaBancariaUtil cuentaBancariaUtil;
 
     @Override
-    public Result<String, String> crearCuentaBancaria(CrearCuentaRequestDTO request) {
+    public Result<CuentaBancariaCreadaResponse, String> crearCuentaBancaria(CrearCuentaRequestDTO request) {
 
-        Optional<CuentaBancaria> cuentaBancaria = cuentaBancariaRepository.findByNumeroCuenta(request.getNumeroCuenta());
-        if(cuentaBancaria.isPresent()) {
-            return Result.failure(List.of("Cuenta bancaria ya existe"), HttpStatus.BAD_REQUEST);
-        }
 
         Optional<Usuario> usuario = usuarioRepository.findByNumeroIdetificacionAndActivoTrue(request.getNumeroIdetificacion());
         if(!usuario.isPresent()) {
             return Result.failure(List.of("Usuario no encontrado"), HttpStatus.BAD_REQUEST);
         }
 
-        CuentaBancaria nuevaCuentaBancaria = CuentaBancaria.builder()   
-                .numeroCuenta(request.getNumeroCuenta())
-                .saldo(request.getSaldo())
-                .tipoCuenta(request.getTipoCuenta())
-                .usuario(usuario.get())
-                .build();
-
-        cuentaBancariaRepository.save(nuevaCuentaBancaria);
-        return Result.success("Cuenta bancaria creada correctamente");
+        CuentaBancaria cuentaGuardada = cuentaBancariaRepository.save(cuentaBancariaMapper.toEntity(request, usuario.get(), cuentaBancariaUtil));
+        return Result.success(CuentaBancariaCreadaResponse.builder().numeroCuenta(cuentaGuardada.getNumeroCuenta()).build());
     }
 
     @Override
