20eb7aa8638e931d7f59fb96463e375d891d8dd1
Charlinson <charlinson2013@gmail.com>
2025-02-06 14:21:22 -0500

    feat: añade @Builder al DTO

--- src/main/java/com/banco/cuenta_bancaria/dto/request/RegistrarUsuarioRequestDTO.java
+++ src/main/java/com/banco/cuenta_bancaria/dto/request/RegistrarUsuarioRequestDTO.java
@@ -6,12 +6,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
-@ToString
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegistrarUsuarioRequestDTO {
